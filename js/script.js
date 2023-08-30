7fd8ec8179a0cdc731849262d654617a069f101c
mdabarik <mdabarik19@gmail.com>
2023-08-30 17:18:25 +0600

    modal added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,66 @@
 
 
+const showModalInfo = async (id) => {
+    let url = `https://openapi.programming-hero.com/api/ai/tool/0${id}`;
+    if (id >= 10) {
+        url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    }
+
+    // {id: '01', tool_name: 'ChatGPT', description: "ChatGPT is an AI-powered chatbot platform that use…'s GPT technology to simulate human conversation.", website: 'https://openai.com/', logo: 'https://cdn.openai.com/research-covers/gpt-3-social.jpg', …}
+
+    fetch(url)
+     .then(res => res.json())
+     .then(responseData => {
+        const data = responseData.data;
+        console.log(data);
+        const container = document.getElementById('my_modal');
+        container.innerHTML = `
+            <form method="dialog" class="modal-box max-w-[70%]">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                <div class="p-3 flex flex-col md:flex-row rounded-xl gap-3">
+                    <div class="border bg-[#EB57570D] border-red-300 rounded-lg">
+                        <h2 class="text-2xl m-3">${data.description}</h2>
+                        <div class="flex flex-col md:flex-row  justify-around gap-4 m-3">
+                            <p class="bg-white rounded-2xl text-[#03A30A] px-8 py-2 text-center">
+                                ${data.pricing[0].price} <br> ${data.pricing[0].plan}
+                            </p>
+                            <p class="bg-white rounded-2xl text-[#F28927] px-8 py-2 text-center">
+                                ${data.pricing[1].price} <br> ${data.pricing[1].plan}
+                            </p>
+                            <p class="bg-white rounded-2xl text-[#EB5757] px-8 py-2 text-center">
+                                ${data.pricing[1].price} <br> ${data.pricing[1].plan}
+                            </p>
+                        </div>
+                        <div class="flex flex-row p-4">
+                            <div>
+                                <h2 class="text-3xl font-semibold">Features</h2>
+                                <ul class="list-disc m-4">
+                                    <li>${data.features[1]?.feature_name}</li>
+                                    <li>${data.features[2]?.feature_name}</li>
+                                    <li>${data.features[3]?.feature_name}</li>
+                                </ul>
+                            </div>
+                            <div >
+                                <h2 class="text-3xl font-semibold">Integrations</h2>
+                                <ul class="list-disc m-4">
+                                    <li>${data.integrations[0]}</li>
+                                    <li>${data.integrations[1]}</li>
+                                    <li>${data.integrations[2]}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border border-gray-200 rounded-lg flex flex-col justify-center items-center p-4 mx-6 gap-y-3">
+                        <img class="rounded-xl" src="${data.image_link[0]}" alt="image">
+                        <h2 class="text-2xl font-semibold text-center">Hi, how are you doing today?</h2>
+                        <p class="text-[#585858] font-thin text-center">I'm doing well, thank you for asking. How can i assist you today?</p>
+                    </div>
+                </div>
+            </form>
+        `;
+    })
+}
+
 const displayTools = (tools) => {
     // 0. get div container
     // 1. create a div element and add class for each tool in tools
@@ -31,7 +92,7 @@ const displayTools = (tools) => {
                         <span class="text-[#585858] ml-3">${tool.published_in}</span>
                     </div>
                 </div>
-                <div class="bg-gray-100 rounded-full p-4 cursor-pointer">
+                <div class="bg-gray-100 rounded-full p-4 cursor-pointer" onclick="my_modal.showModal(); showModalInfo(${tool.id})">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                         fill="none">
                         <path d="M4.5 12H19.5M19.5 12L12.75 5.25M19.5 12L12.75 18.75" stroke="#EB5757"
@@ -71,7 +132,11 @@ const sortByDate = function() {
     })
 }
 
-
-
+// // for no internet connections
+// const el = document.getElementById('tools-container');
+// if (el.innerText == '') {
+//     el.innerText = 'Ooops! Please check your internet connections!';
+// }
 
 showAllData();
+
